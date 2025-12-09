20fcb8dd7e2c38d765b04d747c6b99d2f8cd04fb
german23.03.09@gmai.com <german23.03.09@gmail.com>
2025-12-09 16:23:52 -0300

    se arreglo home page nombre

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -13,7 +13,7 @@ import { useUsuario } from "../context/usuarioContext/useUsuarioContext";
 import { usePortafolio } from "../hooks/usePortfolio.js";
 import { useTransacciones } from "../hooks/useTransacciones";
 import { useEffect,useState } from "react";
-
+ 
 export function HomePage() { 
    const {cotizaciones} = useCotizacionesContext()
    const { usuario } = useUsuario() ?? {};
